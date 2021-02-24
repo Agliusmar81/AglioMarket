35306c131e0ae194f40b13ee14a4a84e32fbc1be
Agliusmar81 <marcoa.81@hotmail.it>
2021-02-24 08:54:22 +0100

    validazione

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,6 +1,13 @@
 class Content < ApplicationRecord
-  belongs_to :user
-  has_attached_file :cover
+
+	extend FriendlyId
+  friendly_id :titolo, use: :slugged
+
+	belongs_to :user
+
+	has_many :sales
+
+	has_attached_file :cover
   validates_attachment_content_type :cover, content_type: /\Aimage\/.*\z/,
   message: "Solo le immagini sono supportate"
 
@@ -8,5 +15,11 @@ class Content < ApplicationRecord
   has_attached_file :allegato
   validates_attachment_content_type :allegato, :content_type => [ /^image\/(png|gif|jpeg)/,'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document','application/mspowerpoint','application/vnd.ms-powerpoint','application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/pdf', 'application/msexcel','application/vnd.ms-excel','application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','audio/mpeg', 'audio/mp3' ],
   message: "Formato non supportato"
+
+  validates :titolo, :descrizione, :price, presence: true   
+  validates :price, numericality: { greater_than: 0.49 }
+  validates :cover, attachment_presence: true
+  validates :allegato, attachment_presence: true
+
 end
 
