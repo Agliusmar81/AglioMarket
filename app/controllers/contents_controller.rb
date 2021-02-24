702b912e0b401de40c8b4db6bd97729655f2ed90
Agliusmar81 <marcoa.81@hotmail.it>
2021-02-24 08:29:08 +0100

    autorizzazioni e associazioni per user e contents

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,69 +1,65 @@
 class ContentsController < ApplicationController
-  before_action :set_content, only: %i[ show edit update destroy ]
+  before_action :set_content, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :check_user, only: [:edit, :update, :destroy]
 
-  # GET /contents or /contents.json
   def index
-    @contents = Content.all
+    @contents = Content.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
   end
 
-  # GET /contents/1 or /contents/1.json
   def show
   end
 
-  # GET /contents/new
   def new
-    @content = Content.new
+    @content = current_user.contents.build
   end
 
-  # GET /contents/1/edit
   def edit
   end
 
-  # POST /contents or /contents.json
   def create
-    @content = Content.new(content_params)
+    @content = current_user.contents.build(content_params)
 
     respond_to do |format|
       if @content.save
-        format.html { redirect_to @content, notice: "Content was successfully created." }
-        format.json { render :show, status: :created, location: @content }
+        format.html { redirect_to @content, notice: 'Il contenuto è stato creato con successo.' }
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @content.errors, status: :unprocessable_entity }
+        format.html { render :new }
       end
     end
   end
 
-  # PATCH/PUT /contents/1 or /contents/1.json
   def update
     respond_to do |format|
       if @content.update(content_params)
-        format.html { redirect_to @content, notice: "Content was successfully updated." }
-        format.json { render :show, status: :ok, location: @content }
+        format.html { redirect_to @content, notice: 'Il contenuto è stato aggiornato con successo.' }
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @content.errors, status: :unprocessable_entity }
+        format.html { render :edit }
       end
     end
   end
 
-  # DELETE /contents/1 or /contents/1.json
   def destroy
     @content.destroy
     respond_to do |format|
-      format.html { redirect_to contents_url, notice: "Content was successfully destroyed." }
-      format.json { head :no_content }
+      format.html { redirect_to contents_url, notice: 'Il contenuto è stato eliminato.' }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_content
-      @content = Content.find(params[:id])
+      @content = Content.friendly.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+
     def content_params
-      params.require(:content).permit(:titolo, :descrizione, :price)
+      params.require(:content).permit(:titolo, :descrizione, :price, :cover, :allegato)
     end
+
+    def check_user
+      if current_user != @content.user
+        redirect_to root_url, alert: "Scusa, non hai accesso a questa pagina"
+      end
+    end      
 end
