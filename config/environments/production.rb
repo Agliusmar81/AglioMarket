c73abbcafaba9f618b1d170b76f7f1618980b8e0
Agliusmar81 <marcoa.81@hotmail.it>
2021-04-04 11:11:51 +0200

    modificati production e paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,14 +40,20 @@ Rails.application.configure do
     config.active_record.dump_schema_after_migration = false
     
     config.paperclip_defaults = {
+
       storage: :s3,
+      
+      s3_region: ENV["eu-central-1"],
+      
       s3_credentials: {
-        bucket: ENV.fetch('S3_BUCKET_NAME'),
-        access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-        secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-        s3_region: ENV.fetch('AWS_REGION'),
-      }
-    }
+      
+      s3_host_name: ENV['s3.eu-central-1.amazonaws.com'],
+      
+      bucket: ENV["agliomarket"],
+      
+      access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      
+      secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
 
 end
 
