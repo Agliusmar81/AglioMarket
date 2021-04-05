cd6bded9a0211c1b9bdf76fbf3e5d994ce8e1315
UpdatedUserName <marcoa.81@hotmail.it>
2021-04-05 09:35:42 +0200

    rimosso user_controler

--- app/models/user_controller.rb
+++
@@ -1,8 +0,0 @@
-  # GET /users/new
-  def new
-    @s3_direct_post = S3_BUCKET.presigned_post(
-      key:                   "uploads/#{SecureRandom.uuid}/${filename}", 
-      acl:                   :public_read,
-      success_action_status: 201)
-    @user = User.new
-  end
